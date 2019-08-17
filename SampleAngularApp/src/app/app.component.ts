28c2f1b10b723be7b3a358284aa8c31557bb8345
Gaurav <gaurav360@outlook.com>
2019-08-18 03:32:41 +0530

    added menu

--- SampleAngularApp/src/app/app.component.ts
+++ SampleAngularApp/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { RouterService } from './services/Router.Service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SampleAngularApp';
+  title = 'Record Manager';
 
   constructor(private authService:AuthenticationService, private routerService:RouterService){}
 
